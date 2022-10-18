d05c91df08f124e310d45777c3ab6aef72b6efbc
Siddesh Sambasivam <plutocrat45@gmail.com>
2022-10-18 11:19:26 +0800

    code format w/ black

--- src/dashboard/model_handler.py
+++ src/dashboard/model_handler.py
@@ -9,18 +9,21 @@ from src.models.nesymres import NeSymRes
 def get_model(model_name: str):
     """Loads the model with the given name."""
 
-    if model_name == "gplearn":               
+    if model_name == "gplearn":
         model = Gplearn(function_set=FUNCTION_SET, tournament_size=10, verbose=0)
 
     elif model_name == "dsr":
         model = DSR("configs/dsr_config.json")
-    
-    elif model_name == "dsr-gp":        
-        model = DSR("configs/dsr_gp_config.json")        
+
+    elif model_name == "dsr-gp":
+        model = DSR("configs/dsr_gp_config.json")
 
     elif model_name == "aifeynman":
         model = AIFeynman(
-            "add,sub,mul,div,sin,cos,exp,log,sqrt", NN_epochs=1, max_time=60, BF_try_time=5
+            "add,sub,mul,div,sin,cos,exp,log,sqrt",
+            NN_epochs=1,
+            max_time=60,
+            BF_try_time=5,
         )
 
     elif model_name == "nesymres":
@@ -28,7 +31,7 @@ def get_model(model_name: str):
             "configs/NeSymRes/100M.ckpt",
             "configs/NeSymRes/config.yaml",
             "configs/NeSymRes/eq_setting.json",
-        )        
+        )
 
     else:
         raise ValueError(f"Unknown model name: {model_name}")
--- src/dashboard/pages.py
+++ src/dashboard/pages.py
@@ -9,6 +9,7 @@ from matplotlib import pyplot as plt
 from src.dataset import Equation
 from src.dashboard.model_handler import get_model
 
+
 def render_algebraic_page():
     """Render the algebraic representation page"""
 
@@ -19,11 +20,10 @@ def render_algebraic_page():
                 "max": 5,
                 "min": -5,
             }
-            
-        return support
-    
-    st.write("#### Evaluation metric")
 
+        return support
+
+    st.write("#### Evaluation metric")
 
     st.write("### Algebraic form of representation")
 
@@ -35,39 +35,35 @@ def render_algebraic_page():
             "Genetic Programming",
             "Neural Guided Genetic Programming",
             "Neural Symbolic Regression",
-        )
+        ),
     )
 
     modelToName = {
-        "AI-Feynman":"aifeynman",
-        "Deep Symbolic Regression":"dsr",
-        "Genetic Programming":"gplearn",
-        "Neural Guided Genetic Programming":"dsr-gp",
-        "Neural Symbolic Regression":"nesymres",
+        "AI-Feynman": "aifeynman",
+        "Deep Symbolic Regression": "dsr",
+        "Genetic Programming": "gplearn",
+        "Neural Guided Genetic Programming": "dsr-gp",
+        "Neural Symbolic Regression": "nesymres",
     }
 
     func = st.text_input(
-        "Please enter a function, with variables ranging from x0 to x3", 
-        "2*sin(x0) + 5*x1",         
-        key="input_eq"
+        "Please enter a function, with variables ranging from x0 to x3",
+        "2*sin(x0) + 5*x1",
+        key="input_eq",
     )
 
     with st.expander("Data generation options"):
-            
+
         number_of_pts = st.slider(
-            "Pick the number of support points", 
-            min_value=10, 
-            max_value=500, 
-            value=50, 
-            step=10
+            "Pick the number of support points",
+            min_value=10,
+            max_value=500,
+            value=50,
+            step=10,
         )
 
         noise = st.slider(
-            "Pick the noise level", 
-            min_value=0.0, 
-            max_value=0.1, 
-            value=0.0, 
-            step=0.01
+            "Pick the noise level", min_value=0.0, max_value=0.1, value=0.0, step=0.01
         )
 
     try:
@@ -75,21 +71,21 @@ def render_algebraic_page():
         support = generate_support_dict(expr.free_symbols)
 
         eq = Equation(
-            expr=expr, 
+            expr=expr,
             variables=expr.free_symbols,
-            support=support, 
-            number_of_points=number_of_pts
-            )
+            support=support,
+            number_of_points=number_of_pts,
+        )
 
     except Exception as e:
         st.error(f"Please enter a valid sympy function: {str(e)}")
-        return     
-    
-    x,y = eq.get_data(noise)
+        return
+
+    x, y = eq.get_data(noise)
     st.write(f"Generated dataset for noise level {noise}, with {number_of_pts} points")
 
     if st.button("Train model"):
-        
+
         if model == "AI-Feynman":
             model = get_model(modelToName[model])
         elif model == "Neural Guided Genetic Programming":
@@ -101,32 +97,34 @@ def render_algebraic_page():
         elif model == "Genetic Programming":
             model = get_model(modelToName[model])
 
-        with st.spinner("Fitting the data..."):            
-            model.fit(x,y)
+        with st.spinner("Fitting the data..."):
+            model.fit(x, y)
 
         st.success("Model trained!")
 
         y_pred = model.predict(x)
         score = r2_score(y, y_pred)
-        
+
         st.write("The following equation is provided to generate the dataset:")
         st.latex(sp.latex(eq.expr))
         st.write("The following equation is predicted by the model:")
         st.latex(sp.latex(model.equation()))
 
-
         col1, col2, col3 = st.columns(3)
         with col1:
-            st.metric(label="Prediction accuracy", value=f"{str(round(model.score(x,y), 4))}")
-        
+            st.metric(
+                label="Prediction accuracy", value=f"{str(round(model.score(x,y), 4))}"
+            )
+
         with col2:
             st.metric(label="R2 score", value=f"{str(round(score, 4))}")
 
-        with col3:                        
-            st.metric(label="Mean Squared Error (MSE)", value=f"{str(round(mean_squared_error(y, y_pred), 4))}")            
+        with col3:
+            st.metric(
+                label="Mean Squared Error (MSE)",
+                value=f"{str(round(mean_squared_error(y, y_pred), 4))}",
+            )
 
-
-    
 
 def render_differential_page():
     """Render the differential representation page"""
